5773c661b06324350a0cd042adff8db43a9e0a6d
Robb Broome <robb@teamvoice.com>
2012-06-29 09:40:21 -0500

    GHIN parser ftw

--- ghin_parser.rb
+++ ghin_parser.rb
@@ -7,50 +7,51 @@ not_active_count = 0
 require 'csv'
 
 # open the destination file and prepare it to receive CSV data
-CSV.open('golfer_data.csv', 'wb') do |csv|
+CSV.open('ghin_golfer_data.csv', 'wb') do |csv|
 
   # open the data file you made by pasting in from the golf handicap site
   # Handle one line from the file at a time in the variable called 'golfer'
-  File.open('golfscores.txt').each do |golfer|
+  golf_data_file = File.open('ghin_raw_data.txt')
 
+  while true
+    golfer = golf_data_file.gets
+    line_number = 1
+
+    break unless golfer
+    
     # count all golfers
     golfer_count += 1
-
-    # eliminate golfers that are not active or N/H right away
-    not_active_matcher = /(Not Active|N\/H)/
-      if not_active_matcher.match(golfer)
-        not_active_count += 1
-        next
-      end
 
     # get rid of leading and trailing blanks
     golfer.strip!
 
-    # pull out the handicap, which should be the only digits in the data
-    # right now, rejecting handicaps that do not have a digit after the decimal
-    # This is using a regular expression (\d+\.\d) that says:
-    # find one or more digits (d+) followed by a '.', followed by one digit
-    handicap_match = /(\d+\.\d)/.match golfer
+
+    # the first line is the name and club.
+    # we may try to parse out later
+    name_club = golfer
+
+    # get the next line, which is association and handicap
+    assn_and_handicap = golf_data_file.gets
+
+    handicap_match = /HDCP Index: (\d+\.\d)/.match assn_and_handicap
     handicap = handicap_match ? handicap_match.captures.first : nil
 
-    # skip to the next golfer if there is no handicap listed
-    next unless handicap
+    association_match = /(.+)\s+(HDCP Index:)/.match assn_and_handicap
+    association = association_match ? association_match.captures.first : nil
+
+    # the next line is just the date
+    effective_date_line = golf_data_file.gets
+    effective_date_match = /Eff Date: (\d\d\/\d\d\/\d\d\d\d)/.match effective_date_line
+    effective_date = effective_date_match ? effective_date_match.captures.first : nil
 
 
-    # matchers for the name and city
-    name_match = /(\w+,\s\w+\s*\w*)\s+(\d+\.\d)/.match golfer
-    city_match = /(.+\d+\.\d)(.*)/.match golfer
-    # get the name and the city, or a blank if the don't exist
-    name = name_match ? name_match.captures.first.strip : nil
-    city = city_match ? city_match.captures.last.strip : ""
-
-    if name
+    if name_club
       # count the match
       match_count += 1
       # write to the screen
-      puts "Matched: #{name} Handicap: #{handicap} City: #{city}"
+      puts "Matched name and club: #{name_club}, Association: #{association},  Handicap: #{handicap}, Effective Date: #{effective_date}"
       # write to the CSV File
-      csv << [name, handicap, city]
+      csv << [name_club, association, handicap, effective_date]
 
     else
       # count the no_match
@@ -63,7 +64,7 @@ end
 
 # this makes Mac OS X open the csv file. It will usually use Numbers to do that,
 # so you should see Numbers magically open.
-`open golfer_data.csv`
+`open ghin_golfer_data.csv`
 
 # write statuses to the screen, for fun!
 puts "there were #{golfer_count} golfers"
